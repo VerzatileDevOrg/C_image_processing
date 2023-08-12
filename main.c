6963ce0261969b54ad03b0c6a306f8f2900343a8
Sommos <Sommosgaming@gmail.com>
2023-08-12 06:01:14 +0100

    added license and test_images

--- main.c
+++ main.c
@@ -2,5 +2,8 @@
 #include "filters/negative_filter.c"
 
 int main() {
+    black_and_white_filter("assets/images/test_image.bmp", "assets/images/black_and_white_test_image.bmp");
+    negative_filter("assets/images/test_image.bmp", "assets/images/negative_test_image.bmp");
+
     return 0;
 }
