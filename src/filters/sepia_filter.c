bc4bb077488235177eaf7843443177325af16d08
Sommos <Sommosgaming@gmail.com>
2023-08-22 00:03:05 +0100

    fixed sepia_filter.c

--- src/filters/sepia_filter.c
+++ src/filters/sepia_filter.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-#define MAX_VALUE 255
+#define MAX_COLOR 255
 
 int sepia_filter(const char *input_file, const char *output_file)
 {
@@ -31,38 +31,37 @@ int sepia_filter(const char *input_file, const char *output_file)
     int pixels_in_image = height * width;
     // check if image has a color table
     unsigned char(*buffer)[3] = malloc(pixels_in_image * sizeof(*buffer));
-    if (buffer == NULL)
-        // read & write image data in chunks until end of file reached
-        for (i = 0; i < pixels_in_image; i++)
+    // read & write image data in chunks until end of file reached
+    for (i = 0; i < pixels_in_image; i++)
+    {
+        r = 0;
+        g = 0;
+        b = 0;
+        buffer[i][2] = getc(file_in); // blue
+        buffer[i][1] = getc(file_in); // green
+        buffer[i][0] = getc(file_in); // red
+        // conversion formula of rgb to sepia
+        r = (buffer[i][0] * 0.393) + (buffer[i][1] * 0.769) + (buffer[i][2] * 0.189);
+        g = (buffer[i][0] * 0.349) + (buffer[i][1] * 0.686) + (buffer[i][2] * 0.168);
+        b = (buffer[i][0] * 0.272) + (buffer[i][1] * 0.534) + (buffer[i][2] * 0.131);
+        // check if value is greater than 255
+        if (r > MAX_COLOR)
         {
-            r = 0;
-            g = 0;
-            b = 0;
-            buffer[i][2] = getc(file_in); // blue
-            buffer[i][1] = getc(file_in); // green
-            buffer[i][0] = getc(file_in); // red
-            // conversion formula of rgb to sepia
-            r = (buffer[i][0] * 0.393) + (buffer[i][1] * 0.769) + (buffer[i][2] * 0.189);
-            g = (buffer[i][0] * 0.349) + (buffer[i][1] * 0.686) + (buffer[i][2] * 0.168);
-            b = (buffer[i][0] * 0.272) + (buffer[i][1] * 0.534) + (buffer[i][2] * 0.131);
-            // check if value is greater than 255
-            if (r > MAX_VALUE)
-            {
-                r = MAX_VALUE;
-            }
-            if (g > MAX_VALUE)
-            {
-                g = MAX_VALUE;
-            }
-            if (b > MAX_VALUE)
-            {
-                b = MAX_VALUE;
-            }
-            // write sepia image data to output file
-            putc(b, file_out);
-            putc(g, file_out);
-            putc(r, file_out);
+            r = MAX_COLOR;
         }
+        if (g > MAX_COLOR)
+        {
+            g = MAX_COLOR;
+        }
+        if (b > MAX_COLOR)
+        {
+            b = MAX_COLOR;
+        }
+        // write sepia image data to output file
+        putc(b, file_out);
+        putc(g, file_out);
+        putc(r, file_out);
+    }
     fclose(file_out);
     fclose(file_in);
 
