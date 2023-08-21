02f732bdf5994d7d4beedd73bd1c708c6e0ffd75
Sommos <Sommosgaming@gmail.com>
2023-08-21 21:05:06 +0100

    reverted error check change

--- C_image_processing/filters/sepia_filter.c
+++ C_image_processing/filters/sepia_filter.c
@@ -31,48 +31,50 @@ int sepia_filter(const char *inputFile, const char *outputFile)
     int height = *(int *)&byte[18];
     int width = *(int *)&byte[22];
     int bitDepth = *(int *)&byte[28];
-    int size = height * width;
+    int pixelsInImage = height * width;
 
     // check if image has a color table
-    unsigned char buffer[size][3];
+    // store image data in buffer
+    unsigned char(*buffer)[3] = malloc(pixelsInImage * sizeof(*buffer));
+    if (buffer == NULL)
 
-    // read & write image data in chunks until end of file reached
-    for (i = 0; i < size; i++)
-    {
-        r = 0;
-        g = 0;
-        b = 0;
-
-        buffer[i][2] = getc(fileIn); // blue
-        buffer[i][1] = getc(fileIn); // green
-        buffer[i][0] = getc(fileIn); // red
-
-        // conversion formula of rgb to sepia
-        r = (buffer[i][0] * 0.393) + (buffer[i][1] * 0.769) + (buffer[i][2] * 0.189);
-        g = (buffer[i][0] * 0.349) + (buffer[i][1] * 0.686) + (buffer[i][2] * 0.168);
-        b = (buffer[i][0] * 0.272) + (buffer[i][1] * 0.534) + (buffer[i][2] * 0.131);
-
-        // check if value is greater than 255
-        if (r > MAX_VALUE)
+        // read & write image data in chunks until end of file reached
+        for (i = 0; i < pixelsInImage; i++)
         {
-            r = MAX_VALUE;
-        }
+            r = 0;
+            g = 0;
+            b = 0;
 
-        if (g > MAX_VALUE)
-        {
-            g = MAX_VALUE;
-        }
+            buffer[i][2] = getc(fileIn); // blue
+            buffer[i][1] = getc(fileIn); // green
+            buffer[i][0] = getc(fileIn); // red
 
-        if (b > MAX_VALUE)
-        {
-            b = MAX_VALUE;
-        }
+            // conversion formula of rgb to sepia
+            r = (buffer[i][0] * 0.393) + (buffer[i][1] * 0.769) + (buffer[i][2] * 0.189);
+            g = (buffer[i][0] * 0.349) + (buffer[i][1] * 0.686) + (buffer[i][2] * 0.168);
+            b = (buffer[i][0] * 0.272) + (buffer[i][1] * 0.534) + (buffer[i][2] * 0.131);
 
-        // write sepia image data to output file
-        putc(b, fileOut);
-        putc(g, fileOut);
-        putc(r, fileOut);
-    }
+            // check if value is greater than 255
+            if (r > MAX_VALUE)
+            {
+                r = MAX_VALUE;
+            }
+
+            if (g > MAX_VALUE)
+            {
+                g = MAX_VALUE;
+            }
+
+            if (b > MAX_VALUE)
+            {
+                b = MAX_VALUE;
+            }
+
+            // write sepia image data to output file
+            putc(b, fileOut);
+            putc(g, fileOut);
+            putc(r, fileOut);
+        }
 
     fclose(fileOut);
     fclose(fileIn);
